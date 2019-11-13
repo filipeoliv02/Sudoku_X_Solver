49558800465daec5777377607d57fac804a5f03e
Filipe Teixeira <filipeh291@gmail.com>
2019-11-13 18:58:16 +0000

    super dinamico

--- fileio.c
+++ fileio.c
@@ -5,6 +5,8 @@ void load_boards(Boards *b) {
     b->num = 0;
     b->sizes = NULL;
     b->board = NULL;
+    int offset = 0;
+
 
     FILE *fp = fopen("board.txt", "r");
     if (fp != NULL) {
@@ -12,13 +14,19 @@ void load_boards(Boards *b) {
             if (strcmp(line, "\n") != 0) {
                 b->sizes = resizeArray(b->sizes, b->num, b->num + 1);
                 b->board = resizeBoards(b->board, b->num, b->num + 1);
-                //sscanf(line,"%d", &b->sizes[b->num]);
-                b->sizes[b->num] = line[0] - '0';
+                sscanf(line, "%d", &b->sizes[b->num]);
+
                 b->board[b->num] = createBoard(b->board[b->num], b->sizes[b->num]);
+
+                offset = b->sizes[b->num] > 9 ? 3 : 2;
                 for (int i = 0; i < b->sizes[b->num]; i++) {
+
                     for (int j = 0; j < b->sizes[b->num]; j++) {
-                        int *boardLine = *(b->board[b->num] + i);
-                        *(boardLine + j) = line[(i * b->sizes[b->num] + j) * 2 + 2] - '0';
+                        sscanf(&line[(i * b->sizes[b->num] + j) * 2 + offset], "%d", &b->board[b->num][i][j]);
+                        if (b->board[b->num][i][j] > 9) {
+                            offset++;
+                        }
+
                     }
                 }
                 b->num++;
@@ -27,3 +35,4 @@ void load_boards(Boards *b) {
         fclose(fp);
     }
 }
+
